ad7ace5bc39fdba869e315ff89f481cf122af34a
Omar Linux <omarverg37@proton.me>
2024-03-19 16:40:58 +0100

    The project named exercise5  was implemented

---
+++ exercises/src/exercise5/App5.java
@@ -0,0 +1,28 @@
+/*
+ * Write a Java program to create a class called "Rectangle" with width and height attributes.
+ * Calculate the area and perimeter of the rectangle.
+ * 
+ */
+
+package exercise5;
+
+public class App5 {
+    public static void main(String[] args) {
+
+        Rectangle r1 = new Rectangle(10.3f, 15.6f);
+
+        System.out.println("\n"+r1.toString());
+        System.out.println("\nThe perimeter of the rectangle is: "+r1.getPerimeter());
+        System.out.println("\nThe area of the rectangle is: "+r1.getArea());
+
+        System.out.println("\n---------------------------------------------------------------");
+
+        r1.setHeight(155);
+        r1.setWidth(20);
+
+        System.out.println("\n"+r1.toString());
+        System.out.println("\nThe perimeter of the rectangle is: "+r1.getPerimeter());
+        System.out.println("\nThe area of the rectangle is: "+r1.getArea());
+
+    }
+}
